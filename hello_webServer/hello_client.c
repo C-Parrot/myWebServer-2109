a9895a71a69534eb819ce61a2c040c8c0bc7731f
Cparrot <1966487388@qq.com>
2021-09-15 22:05:02 +0800

    hello_server

--- hello_webServer/hello_client.c
+++ hello_webServer/hello_client.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#define BUF_SIZE 1024
 
+#define BUF_SIZE 1024
 void error_handling(char *message);
 
 int main(int argc, char* argv[]){
@@ -15,11 +15,26 @@ int main(int argc, char* argv[]){
     sock = socket(PF_INET, SOCK_STREAM, 0);
     char message[30];
 
+    if(argc!=3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+    
+    //用于连接服务端的套接字
+    sock=socket(PF_INET, SOCK_STREAM, 0);   
+	if(sock==-1)
+		error_handling("socket() error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port = hton(atoi(argv[2]));
+    serv_addr.sin_port = htons(atoi(argv[2]));
 
-    connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
+    //向服务端连接请求，建立连接
+    if(connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error!");
+    else
+		puts("Connected...........");
 
     while(1){
         fputs("Input message(Q to quit):" ,stdout);
@@ -31,12 +46,11 @@ int main(int argc, char* argv[]){
         int str_len = read(sock, message, BUF_SIZE -1);//为什么-1
         message[str_len] = 0;//?
         printf("Message from server:%s", message);
-
     }
 
+    //关闭连接
     close(sock);
     return 0;
-
 }
 
 void error_handling(char* message){
--- hello_webServer/hello_server.c
+++ hello_webServer/hello_server.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 
 #define BUF_SIZE 1024
+void error_handling(char *message);
 
 int main(int argc, char * argv[]){
     int serv_sock, clnt_sock;
@@ -13,19 +14,30 @@ int main(int argc, char * argv[]){
     socklen_t clnt_addr_size;
     char message[BUF_SIZE];
 
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0);//protocol family.协议族-类型-最终协议，PF_INET:ipv4协议族
+    if(argc!=2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+    //创建套接字
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);//PF: protocol family.协议族-类型-最终协议，PF_INET:ipv4协议族
     if(serv_sock == -1)
-        error_handing("socket() error");
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;//地址族，address family，基于ipv4的地址族,ipv4网络协议中使用的地址族
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);//h表示主机，n表示network
-    serv_addr.sin_port = hton(atoi(argv[1]));
+        error_handling("socket() error");
     
+    //初始化结构体变量
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;//地址族，AF: address family，ipv4网络协议中使用的地址族
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);//h表示主机，n表示network
+    serv_addr.sin_port = htons(atoi(argv[1]));
+    
+    //套接字地址分配
     if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
-        error_handing("bind() error");
+        error_handling("bind() error");
+
+    //接收端进入等待连接请求状态
     //第二个参数为等待队列大小，频繁接收请求的Web服务器端至少应为15。另外，连接请求队列的大小始终根据实验结果而定
     if(listen(serv_sock, 5) == -1)
-        error_handing("listen() error");
+        error_handling("listen() error");
     
     clnt_addr_size = sizeof(clnt_addr);
 
@@ -33,39 +45,21 @@ int main(int argc, char * argv[]){
         //accept会从半连接队列中取1个连接请求，若等待队列为空，则accept函数不会返回
         clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
         if(clnt_sock == -1) error_handling("accept() error");
-        else printf("Connected client ");
+        else printf("Connected client %d \n", i+1);
         int str_len;
         while((str_len = read(clnt_sock, message, BUF_SIZE)) != 0){
-            write(clnt_sock, message, str_len);
+            write(clnt_sock, message, str_len);//将收到的数据再返回客户端
         }
         close(clnt_sock);
     }
-
 
     close(serv_sock);
     return 0;
 
 }
-/*
-clnt_sd=accept(serv_sd, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
-	
-	while(1)
-	{
-		read_cnt=fread((void*)buf, 1, BUF_SIZE, fp);
-		if(read_cnt<BUF_SIZE)
-		{
-			write(clnt_sd, buf, read_cnt);
-			break;
-		}
-		write(clnt_sd, buf, BUF_SIZE);
-	}
-	
-	shutdown(clnt_sd, SHUT_WR);	
-	read(clnt_sd, buf, BUF_SIZE);
-	printf("Message from client: %s \n", buf);
-	
-	fclose(fp);
-	close(clnt_sd); close(serv_sd);
-	return 0;
-
-    */
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
